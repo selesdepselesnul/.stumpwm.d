b6fe5ff6ee680b7056c23095b4f6de28f0e27f4a
selesdepselesnul <selesdepselesnul@gmail.com>
2017-11-11 10:40:15 +0700

    web-jump github trending

--- init.lisp
+++ init.lisp
@@ -5,15 +5,15 @@
 (swank:create-server :port 4004
                      :style swank:*communication-style*
                      :dont-close t)
-;; Web jump (works for google and youtube)
+;; Web jump
 (defmacro make-web-jump (name prefix)
   `(defcommand ,(intern name) (search) ((:rest ,(concatenate 'string name " search: ")))
-               (substitute #\+ #\Space search)
-               (run-shell-command (concatenate 'string ,prefix search))))
-
+     (substitute #\+ #\Space search)
+     (run-shell-command (concatenate 'string ,prefix search))))
 
 (make-web-jump "google" "google-chrome-stable http://www.google.com/search?q=")
 (make-web-jump "youtube" "google-chrome-stable https://www.youtube.com/results?search_query=")
+(make-web-jump "github-trending" "google-chrome-stable https://github.com/trending/")
 
 (stumpwm:define-key stumpwm:*root-map* (stumpwm:kbd "c") "exec xfce4-terminal")
 
